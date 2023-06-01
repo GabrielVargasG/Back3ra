0c7227f2181ed82bc369ce3162e0f31aff222f41
gab2112 <gabriel.vgonzalez@alumnos.udg.mx>
2023-06-01 15:03:44 -0600

    cambios

--- models/index.js
+++ models/index.js
@@ -1,11 +1,9 @@
-const Categoria = require('./categoria');
 const Nivel = require('./niveles');
 const Role = require('./role');
 const Server = require('./server');
 const Usuario = require('./usuario');
 
 module.exports = {
-    Categoria,
     Nivel,
     Role,
     Server,
--- models/server.js
+++ models/server.js
@@ -13,7 +13,6 @@ class Server {
         this.paths = {
             auth:       '/api/auth',
             buscar:     '/api/buscar',
-            categorias: '/api/categorias',
             productos:  '/api/productos',
             usuarios:   '/api/usuarios',
             uploads:    '/api/uploads',
@@ -59,10 +58,8 @@ class Server {
         
         this.app.use( this.paths.auth, require('../routes/auth'));
         this.app.use( this.paths.buscar, require('../routes/buscar'));
-        this.app.use( this.paths.categorias, require('../routes/categorias'));
         this.app.use( this.paths.productos, require('../routes/niveles'));
         this.app.use( this.paths.usuarios, require('../routes/usuarios'));
-        this.app.use( this.paths.uploads, require('../routes/uploads'));
         
     }
 
